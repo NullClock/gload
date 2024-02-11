ffa72f0bf5e11bcd2d71afd51a999142777fbabd
NullClock <146401816+NullClock@users.noreply.github.com>
2024-02-11 13:51:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,25 @@ import * as gdr from "https://esm.sh/v135/gdrweb@0.2.0/es2022/gdrweb.mjs";
 
 window.onload = async () => {
   const d2 = document.querySelector('#cwgl2');
+  const lst = prompt("Level String:");
 
   await gdr.Renderer.initTextureInfo(
     "GJ_GameSheet-hd.plist",
     "GJ_GameSheet02-hd.plist"
   );
 
-  let rd = new gdr.Renderer(
-    new gdr.WebGLContext(d2),
-    
+  let renderer = new gdr.Renderer(
+    new gdr.WebGLContext(canvas),
+    "GJ_GameSheet-hd.png",
+    "GJ_GameSheet02-hd.png"
   );
+
+  renderer.camera.x = 0;
+  renderer.camera.y = 0;
+  const levelString = lst;
+  let level = gdr.Level.parse(levelString);
+
+  renderer.on('load', () => {
+    renderer.render(level);
+  });
 }
